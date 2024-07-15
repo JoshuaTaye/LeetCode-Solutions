0cfff9b02c53bfc724145c4e51ea899c86c5b9ad
JoshuaTaye <tayejoshua4@gmail.com>
2024-07-15 23:02:24 +0300

    leetcode solutions

---
+++ numberOfOneBits.191.py
@@ -0,0 +1,43 @@
+def hamming_weight(n: int):
+    if n == 0:
+        return 0
+    i = 0
+    while i < n:
+        if 2 ** i >= n:
+            break
+        else:
+            i += 1
+    final = [0] * i
+    final[0] = 1
+    for j in range(1, i):
+        difference = n - (2 ** (i - j))
+        if difference > 0:
+            k = 0
+            while k < difference:
+                print(k, difference)
+                if 2 ** k == difference:
+                    index = i - k - 1
+                    final[index] = 1
+                    count = 0
+                    for i in final:
+                        if i == 1:
+                            count += 1
+                    return count
+                if (2 ** k) > difference:
+                    index = -1 * k
+                    difference = difference - (2 ** (k - 1))
+                    print(difference, "difference")
+                    final[index] = 1
+                    k = 0
+                    print(final)
+                else:
+                    k += 1
+    count = 0
+    for i in final:
+        if i == 1:
+            count += 1
+    return count
+
+
+# num = int(input())
+print(hamming_weight(15))
